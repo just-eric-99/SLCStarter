3c0cb0a284043a30819adf48b799df491b090c7b
lyh7629 <tom19981122@gmail.com>
2022-03-30 11:06:44 +0800

    Mirror format update

--- src/SLC/SLCEmulatorStarter.java
+++ src/SLC/SLCEmulatorStarter.java
@@ -36,46 +36,46 @@ public class SLCEmulatorStarter extends SLCStarter {
     public static class Emulators extends Application {
         private static SLCEmulatorStarter slcEmulatorStarter;
 
-	//----------------------------------------
-	// start
+        //----------------------------------------
+        // start
         public void start() {
-            launch();
-	} // start
+                launch();
+        } // start
 
-	//----------------------------------------
-	// start
+        //----------------------------------------
+        // start
         public void start(Stage primaryStage) {
-	    Timer timer = null;
-	    SLC slc = null;
-	    BarcodeReaderEmulator barcodeReaderEmulator = null;
-	    TouchDisplayEmulator touchDisplayEmulator = null;
+            Timer timer = null;
+            SLC slc = null;
+            BarcodeReaderEmulator barcodeReaderEmulator = null;
+            TouchDisplayEmulator touchDisplayEmulator = null;
 
-	    // create emulators
-	    try {
-	        timer = new Timer("timer", slcEmulatorStarter);
-	        slc = new SLC("SLC", slcEmulatorStarter);
-	        barcodeReaderEmulator = new BarcodeReaderEmulator("BarcodeReaderDriver", slcEmulatorStarter);
-	        touchDisplayEmulator = new TouchDisplayEmulator("TouchDisplayHandler", slcEmulatorStarter);
+            // create emulators
+            try {
+                timer = new Timer("timer", slcEmulatorStarter);
+                slc = new SLC("SLC", slcEmulatorStarter);
+                barcodeReaderEmulator = new BarcodeReaderEmulator("BarcodeReaderDriver", slcEmulatorStarter);
+                touchDisplayEmulator = new TouchDisplayEmulator("TouchDisplayHandler", slcEmulatorStarter);
 
-		// start emulator GUIs
-                barcodeReaderEmulator.start();
-		touchDisplayEmulator.start();
-	    } catch (Exception e) {
-		System.out.println("Emulators: start failed");
-		e.printStackTrace();
-		Platform.exit();
-	    }
-	    slcEmulatorStarter.setTimer(timer);
-	    slcEmulatorStarter.setSLC(slc);
-	    slcEmulatorStarter.setBarcodeReaderDriver(barcodeReaderEmulator);
-	    slcEmulatorStarter.setTouchDisplayHandler(touchDisplayEmulator);
+                // start emulator GUIs
+                    barcodeReaderEmulator.start();
+                touchDisplayEmulator.start();
+            } catch (Exception e) {
+                System.out.println("Emulators: start failed");
+                e.printStackTrace();
+                Platform.exit();
+            }
+            slcEmulatorStarter.setTimer(timer);
+            slcEmulatorStarter.setSLC(slc);
+            slcEmulatorStarter.setBarcodeReaderDriver(barcodeReaderEmulator);
+            slcEmulatorStarter.setTouchDisplayHandler(touchDisplayEmulator);
 
-	    // start threads
-	    new Thread(timer).start();
-	    new Thread(slc).start();
-            new Thread(barcodeReaderEmulator).start();
-	    new Thread(touchDisplayEmulator).start();
-	} // start
+            // start threads
+            new Thread(timer).start();
+            new Thread(slc).start();
+                new Thread(barcodeReaderEmulator).start();
+            new Thread(touchDisplayEmulator).start();
+        } // start
     } // Emulators
 
 
